ede3f025358d5070a107693113f6ac13814224c9
Javlonbek Abdurasulov <javlonbeabr41@gmail.com>
2023-06-26 20:26:41 +0500

    counter

---
+++ vue-project/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Counter {{ counter }}</h1>
+    <button @click="onIncrement" class="btn btn-success">Incr</button>
+    <button @click="onDecrement" class="btn btn-danger">Decr</button>
+  </div>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      counter: 0,
+    }
+  },
+  methods: {
+    onIncrement(){
+      this.counter += 1;
+    },
+    onDecrement(){
+      this.counter -= 1
+    },
+  },
+}
+</script>
+
+<style>
+</style>
